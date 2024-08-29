dad1238ec8247ad70dfaccf9b71b988bbab0cd61
Novik_Vladislav <vladislavnovik95@gmail.com>
2024-08-29 15:15:54 +0300

    feat: add tape animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,12 +30,21 @@ const config: Config = {
       },
       animation: {
         'ping-large': 'ping-large 1s ease-in-out infinite',
+        'move-left': 'move-left 30s linear infinite',
       },
       keyframes: {
         'ping-large': {
           '75%, 100%': {
             transform: 'scale(3)',
             opacity: '0',
+          },
+        },
+        'move-left': {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '100%': {
+            transform: 'translateX(-50%)',
           },
         },
       },
